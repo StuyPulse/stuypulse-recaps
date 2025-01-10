6c4d46d64a26ebe366f1c3f6c89f756d90488a3a
Daniel Yang <danielwzyang@gmail.com>
2025-01-10 09:42:27 -0500

    fixed event listening for message.im by checking channel type

--- api/slack.ts
+++ api/slack.ts
@@ -14,7 +14,7 @@ export async function POST(request: Request) {
 
     if (requestType === "event_callback") {
         const eventType = body.event.type
-        if (eventType === "app_mention" || eventType == "message.im") {
+        if (eventType === "app_mention" || (eventType == "message" && body.event.channel_type == "im")) {
             await sendGPTResponse(body.event)
             return new Response("Success!", { status: 200 })
         }
