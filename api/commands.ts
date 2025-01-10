a95b1495ac51603c9c066bb46f5b1a7f9c4efa04
Daniel Yang <danielwzyang@gmail.com>
2025-01-10 13:34:10 -0500

    adding basic changeprompt command without change

--- api/commands.ts
+++ api/commands.ts
@@ -1,11 +1,29 @@
+import { WebClient } from "@slack/web-api"
+
 export const config = {
     maxDuration: 30,
 }
 
+const slack = new WebClient(process.env.SLACK_BOT_TOKEN)
+
 export async function POST(request: Request) {
     const formData = await request.formData()
 
-    console.log(`Command: ${formData.get("command")}`)
+    const commandName = formData.get("command")?.toString()
+    const prompt = formData.get("text")?.toString().trim()
+    console.log(`Command: ${commandName}`)
+    console.log(`Argument: ${prompt}`)
 
-    return new Response("OK", { status: 200 })
+    if (!prompt || prompt.length < 50)
+        return new Response("Please provide a prompt with at least 50 characters.")
+
+    const user = formData.get("user_id")?.toString()
+    const channel = formData.get("channel_id")?.toString() || ""
+
+    await slack.chat.postMessage({
+        channel,
+        text: `<@${user}> has changed the prompt to ${prompt}`,
+    })
+    
+    return new Response("Success!", { status: 200 })
 }
