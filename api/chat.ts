89f460802d9b3a055ec5e05258a4a0debdf8dddf
Daniel Yang <danielwzyang@gmail.com>
2025-01-12 22:04:42 -0500

    adding date inclusive range functionality

--- api/chat.ts
+++ api/chat.ts
@@ -12,17 +12,18 @@ const slack = new WebClient(process.env.SLACK_BOT_TOKEN)
 type Event = {
     channel: string
     ts: string
+    text: string
 }
 
 export async function sendGPTResponse(event: Event) {
-    const { channel, ts } = event
+    const { channel, ts, text } = event
 
     try {
         const thread = await slack.conversations.replies({ channel, ts })
         if (thread.messages?.length != 1) 
             return
 
-        const messages = await fetchMessages()
+        const messages = await fetchMessages(text)
         const prompts = await generatePromptFromMessage(messages)
         const gptResponse = await getGPTResponse(prompts)
 
@@ -42,18 +43,27 @@ export async function sendGPTResponse(event: Event) {
     }
 }
 
-async function fetchMessages() {
+async function fetchMessages(text: string) {
     const messages: string[] = []
 
     const recapChannels = process.env.SLACK_RECAP_CHANNELS?.split(" ") || []
 
-    const midnightEST = dayjs().tz('America/New_York').startOf('day').utc()
-    const oldest = String(midnightEST.unix())
+    const dateMatch = text.match(/<@\w+>\s(\d{2}-\d{2}-\d{2})\s(\d{2}-\d{2}-\d{2})/)
+
+    let oldest, latest
+
+    if (dateMatch) {
+        const [, startDate, endDate] = dateMatch
+        oldest = String(dayjs.tz(startDate, "MM-DD-YY", "America/New_York").startOf("day").utc().unix())
+        latest = String(dayjs.tz(endDate, "MM-DD-YY", "America/New_York").endOf("day").utc().unix())
+    } else
+        oldest = String(dayjs().tz("America/New_York").startOf("day").utc().unix())
 
     for (let recapChannel of recapChannels) {
         const response = await slack.conversations.history({
             channel: recapChannel,
             oldest,
+            ...(latest && { latest })
         })
         
         response.messages?.forEach(message => {
